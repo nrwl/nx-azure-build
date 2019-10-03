9dbd1070f0b9571ddae07abd6837a0f1ac3b6e68
Victor Savkin <vic.savkin@gmail.com>
2019-10-03 13:40:14 -0400

    more changes

--- tools/scripts/calculate-commands.js
+++ tools/scripts/calculate-commands.js
@@ -1,16 +1,23 @@
 const execSync = require('child_process').execSync;
 
 const affectedProjects = [
-  execSync(`npx nx affected:apps --base=origin/master~2 --plain`).toString().split(' '),
-  execSync(`npx nx affected:libs --base=origin/master~2 --plain`).toString().split(' ')
+  execSync(`npx nx affected:apps --base=origin/master~2 --plain`)
+    .toString()
+    .split(' '),
+  execSync(`npx nx affected:libs --base=origin/master~2 --plain`)
+    .toString()
+    .split(' ')
 ];
 
-const lint1 = affectedProjects.slice(0, Math.floor(affectedProjects.length / 2));
+const lint1 = affectedProjects.slice(
+  0,
+  Math.floor(affectedProjects.length / 2)
+);
 const lint2 = affectedProjects.slice(Math.floor(affectedProjects.length / 2));
 
 const commands = {
-  lint1: lint1.map(s => `npx nx lint ${s}`),
-  lint2: lint2.map(s => `npx nx lint ${s}`)
+  lint1,
+  lint2
 };
 
 console.log(JSON.stringify(commands));
